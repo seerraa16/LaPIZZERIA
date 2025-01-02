fb2b80ddee52939fd1ae43b167678a5f24e84963
Nacho <isanccal@myuax.com>
2025-01-02 13:29:44 +0100

    Incluyendo las claves de la API y haciendo el gitignore

--- little_italy/little_italy/settings.py
+++ little_italy/little_italy/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+EDAMAM_APP_ID = os.getenv('EDAMAM_APP_ID', '')
+PIZZA_KEY = os.getenv('PIZZA_KEY', '')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es"
 
 TIME_ZONE = "UTC"
 
